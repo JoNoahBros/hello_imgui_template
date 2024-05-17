79cea13ca4151c8af49175d7275a5a709ec1576c
Sabahudin Crnkic <s.crnkic@krasser.at>
2024-05-17 11:21:25 +0200

    Update cpp

--- hello_world.main.cpp
+++ hello_world.main.cpp
@@ -1,9 +1,18 @@
+// #ifndef IMGUI_DEFINE_MATH_OPERATORS
+// #define IMGUI_DEFINE_MATH_OPERATORS
+// #endif // IMGUI_DEFINE_MATH_OPERATORS
+// #ifndef IMPLOT_DISABLE_OBSOLETE_FUNCTIONS
+// #define IMPLOT_DISABLE_OBSOLETE_FUNCTIONS
+//#endif
 #include "hello_imgui/hello_imgui.h"
 #include <emscripten/fetch.h>
 #include <sstream>
 #include <iomanip>
 #include <cstdio>
-#include "implot.h"
+#include "source_implot/implot.h"
+#include "source_implot/implot_internal.h"
+#include <vector>
+
 
 static char szTestText[200];
 
@@ -62,55 +71,80 @@ void sliderValue(int value){
 int slideVal = 24 ;
 double lastTime= 0.0;
 
-void Demo_RealtimePlots() {
-    ImGui::BulletText("Move your mouse to change the data!");
-    ImGui::BulletText("This example assumes 60 FPS. Higher FPS requires larger buffer size.");
-    static ScrollingBuffer sdata1, sdata2;
-    static RollingBuffer   rdata1, rdata2;
-    ImVec2 mouse = ImGui::GetMousePos();
-    static float t = 0;
-    t += ImGui::GetIO().DeltaTime;
-    sdata1.AddPoint(t, mouse.x * 0.0005f);
-    rdata1.AddPoint(t, mouse.x * 0.0005f);
-    sdata2.AddPoint(t, mouse.y * 0.0005f);
-    rdata2.AddPoint(t, mouse.y * 0.0005f);
 
-    static float history = 10.0f;
-    ImGui::SliderFloat("History",&history,1,30,"%.1f s");
-    rdata1.Span = history;
-    rdata2.Span = history;
-
-    static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
-
-    if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1,150))) {
-        ImPlot::SetupAxes(NULL, NULL, flags, flags);
-        ImPlot::SetupAxisLimits(ImAxis_X1,t - history, t, ImGuiCond_Always);
-        ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
-        ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
-        ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y, sdata1.Data.size(), -INFINITY, 0, sdata1.Offset, 2 * sizeof(float));
-        ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2*sizeof(float));
-        ImPlot::EndPlot();
+// utility structure for realtime plot
+struct ScrollingBuffer {
+    int MaxSize;
+    int Offset;
+    ImVector<ImVec2> Data;
+    ScrollingBuffer(int max_size = 2000) {
+        MaxSize = max_size;
+        Offset  = 0;
+        Data.reserve(MaxSize);
     }
-    if (ImPlot::BeginPlot("##Rolling", ImVec2(-1,150))) {
-        ImPlot::SetupAxes(NULL, NULL, flags, flags);
-        ImPlot::SetupAxisLimits(ImAxis_X1,0,history, ImGuiCond_Always);
-        ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
-        ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(), 0, 0, 2 * sizeof(float));
-        ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(), 0, 0, 2 * sizeof(float));
+    void AddPoint(float x, float y) {
+        if (Data.size() < MaxSize)
+            Data.push_back(ImVec2(x,y));
+        else {
+            Data[Offset] = ImVec2(x,y);
+            Offset =  (Offset + 1) % MaxSize;
+        }
+    }
+    void Erase() {
+        if (Data.size() > 0) {
+            Data.shrink(0);
+            Offset  = 0;
+        }
+    }
+};
+// utility structure for realtime plot
+struct RollingBuffer {
+    float Span;
+    ImVector<ImVec2> Data;
+    RollingBuffer() {
+        Span = 10.0f;
+        Data.reserve(2000);
+    }
+    void AddPoint(float x, float y) {
+        float xmod = fmodf(x, Span);
+        if (!Data.empty() && xmod < Data.back().x)
+            Data.shrink(0);
+        Data.push_back(ImVec2(xmod, y));
+    }
+};
+void Demo_LinePlots() {
+    static float xs1[1001], ys1[1001];
+    
+    for (int i = 0; i < 1001; ++i) {
+        xs1[i] = i * 0.001f;
+        ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + (float)ImGui::GetTime() / 10));
+    }
+    static double xs2[20], ys2[20];
+    for (int i = 0; i < 20; ++i) {
+        xs2[i] = i * 1/19.0f;
+        ys2[i] = xs2[i] * xs2[i];
+    }
+    if (ImPlot::BeginPlot("Line Plots")) {
+        ImPlot::SetupAxes("x","y");
+        ImPlot::PlotLine("f(x)", xs1, ys1, 1001);
+        ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+        ImPlot::PlotLine("g(x)", xs2, ys2, 20,ImPlotLineFlags_Segments);
         ImPlot::EndPlot();
     }
 }
 
-// int   bar_data[11] = ua;
-// float x_data[1000] = ur;
-// float y_data[1000] =uref;
+// void Demo_RealtimePlots() {
+//     ImGui::BulletText("Move your mouse to change the data!");
+   
 
-  // ImGui::Begin("My Window");
-  //   if (ImPlot::BeginPlot("My Plot")) {
-  //       ImPlot::PlotBars("My Bar Plot", ur, 11);
-  //       ImPlot::PlotLine("My Line Plot", ua, ubat, 1000);
-        
-  //   ImPlot::EndPlot();
+//     //static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+
+//     if (ImPlot::BeginPlot("##Scrolling")) {
+//         ImPlot::PlotLine("Mouse Y",ua,ubat,100);
+//         ImPlot::EndPlot();
+//     }
+   
+// }
 
 int main(int , char *[]) {   
     auto guiFunction = []() {
@@ -131,13 +165,146 @@ int main(int , char *[]) {
           request();
         }
         ImGui::Text("Values is: %s", szTestText);
-        if (ImGui::Button("Bye!"))                 // Display a button
+        if (ImGui::Button("Bye!")) {               // Display a button
             // and immediately handle its action if it is clicked!
             HelloImGui::GetRunnerParams()->appShallExit = true;
-     };
+      
+        }
+        Demo_LinePlots();
+        // ImGui::CreateContext();
+        // ImPlot::CreateContext();
+
+        
+        // Demo_LinePlots();
+        // ImPlot::DestroyContext();
+        // ImGui::DestroyContext();
+    };
     HelloImGui::Run(guiFunction, "Hello, globe", true);
-  
-}
-ImGui::End();
+    
     return 0;
+    
+    //ImGui::End();
+
 }
+// // Define a utility structure for real-time plot data
+// struct ScrollingBuffer {
+//     int MaxSize;
+//     int Offset;
+//     std::vector<ImVec2> Data;
+
+//     ScrollingBuffer(int max_size = 2000) {
+//         MaxSize = max_size;
+//         Offset = 0;
+//         Data.reserve(MaxSize);
+//     }
+
+//     void AddPoint(float x, float y) {
+//         if (Data.size() < MaxSize)
+//             Data.push_back(ImVec2(x, y));
+//         else {
+//             Data[Offset] = ImVec2(x, y);
+//             Offset = (Offset + 1) % MaxSize;
+//         }
+//     }
+// };
+
+// static char szTestText[200];
+
+// float ua = 0.0f, ur = 0.0f, uref = 0.0f, ubat = 0.0f, pt1000 = 0.0f;
+
+// // Define plotting data buffers
+// static ScrollingBuffer sdata_ua, sdata_ur, sdata_uref, sdata_ubat, sdata_pt1000;
+
+// void downloadSucceeded(emscripten_fetch_t *fetch) {
+//     printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes, fetch->url);
+//     emscripten_fetch_close(fetch);
+
+//     // Copy received data to szTestText and parse numeric values
+//     memcpy(szTestText, fetch->data, fetch->numBytes);
+//     szTestText[fetch->numBytes] = '\0';
+//     std::stringstream data(szTestText);
+
+//     // Read and assign fetched values
+//     data >> ua >> ur >> uref >> ubat >> pt1000;
+
+//     // Add fetched values to plotting buffers
+//     sdata_ua.AddPoint(ImGui::GetTime(), ua);
+//     sdata_ur.AddPoint(ImGui::GetTime(), ur);
+//     sdata_uref.AddPoint(ImGui::GetTime(), uref);
+//     sdata_ubat.AddPoint(ImGui::GetTime(), ubat);
+//     sdata_pt1000.AddPoint(ImGui::GetTime(), pt1000);
+// }
+
+// void downloadFailed(emscripten_fetch_t *fetch) {
+//     printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url, fetch->status);
+//     emscripten_fetch_close(fetch);
+// }
+
+// void request() {
+//     emscripten_fetch_attr_t attr;
+//     emscripten_fetch_attr_init(&attr);
+//     strcpy(attr.requestMethod, "GET");
+//     attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+//     attr.onsuccess = downloadSucceeded;
+//     attr.onerror = downloadFailed;
+//     emscripten_fetch(&attr, "api/val");
+// }
+
+// void Demo_RealtimePlots() {
+//     ImGui::BulletText("Real-time Data Plots");
+
+//     // Plot UA over time
+//     if (ImPlot::BeginPlot("UA vs. Time", nullptr, nullptr, ImVec2(-1, 150))) {
+//         ImPlot::PlotLine("UA", &sdata_ua.Data[0].x, &sdata_ua.Data[0].y, sdata_ua.Data.size(), sdata_ua.Offset, sizeof(ImVec2));
+//         ImPlot::EndPlot();
+//     }
+
+//     // Plot UR over time
+//     if (ImPlot::BeginPlot("UR vs. Time", nullptr, nullptr, ImVec2(-1, 150))) {
+//         ImPlot::PlotLine("UR", &sdata_ur.Data[0].x, &sdata_ur.Data[0].y, sdata_ur.Data.size(), sdata_ur.Offset, sizeof(ImVec2));
+//         ImPlot::EndPlot();
+//     }
+
+//     // Plot UREF over time
+//     if (ImPlot::BeginPlot("UREF vs. Time", nullptr, nullptr, ImVec2(-1, 150))) {
+//         ImPlot::PlotLine("UREF", &sdata_uref.Data[0].x, &sdata_uref.Data[0].y, sdata_uref.Data.size(), sdata_uref.Offset, sizeof(ImVec2));
+//         ImPlot::EndPlot();
+//     }
+
+//     // Plot UBAT over time
+//     if (ImPlot::BeginPlot("UBAT vs. Time", nullptr, nullptr, ImVec2(-1, 150))) {
+//         ImPlot::PlotLine("UBAT", &sdata_ubat.Data[0].x, &sdata_ubat.Data[0].y, sdata_ubat.Data.size(), sdata_ubat.Offset, sizeof(ImVec2));
+//         ImPlot::EndPlot();
+//     }
+
+//     // Plot PT1000 over time
+//     if (ImPlot::BeginPlot("PT1000 vs. Time", nullptr, nullptr, ImVec2(-1, 150))) {
+//         ImPlot::PlotLine("PT1000", &sdata_pt1000.Data[0].x, &sdata_pt1000.Data[0].y, sdata_pt1000.Data.size(), sdata_pt1000.Offset, sizeof(ImVec2));
+//         ImPlot::EndPlot();
+//     }
+// }
+
+// int main(int, char *[]) {
+//     auto guiFunction = []() {
+//         ImGui::Text("Hello, world!");
+
+//         // Display fetched values
+//         ImGui::Text("UA: %.2f | UR: %.2f | UREF: %.2f | UBAT: %.2f | PT1000: %.2f", ua, ur, uref, ubat, pt1000);
+
+//         if (ImGui::Button("Request Data")) {
+//             request();
+//         }
+
+//         // Real-time plots
+//         //Demo_RealtimePlots();
+
+//         // Exit button
+//         if (ImGui::Button("Exit")) {
+//             HelloImGui::GetRunnerParams()->appShallExit = true;
+//         }
+//     };
+
+//     HelloImGui::Run(guiFunction, "Real-time Data Visualization", true);
+    
+//     return 0;
+// }
